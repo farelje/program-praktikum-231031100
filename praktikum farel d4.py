8d3bd3a2b3662ea30f90c306d9169082fc94bab7
farelje <153708099+farelje@users.noreply.github.com>
2023-12-18 20:32:50 +0800

    Add files via upload

---
+++ praktikum farel d4.py
@@ -0,0 +1,39 @@
+Python 3.11.5 (v3.11.5:cce6ba91b3, Aug 24 2023, 10:50:31) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: /Users/muhammadfarelshahizidanlefrand/Downloads/praktikum-d4 (1).py =
+Praktikum-d4
+
+[2, 3, 1, 0, 3, 1, 1, 1, 6]
+item indeks:0 2
+item indeks:3 0
+item indeks:terakhir 6
+item indeks:terakhir (-1) 6
+item indeks:pertama (-9)2
+item indeks:1 (-8)3
+item indeks:5 (-4)1
+item indeks:1,2,3 [3, 1, 0]
+item indeks:1,2,... [3, 1, 0, 3, 1, 1, 1, 6]
+item indeks:3,4,... [0, 3, 1, 1, 1, 6]
+item indeks:0,1,2 [2, 3, 1]
+item indeks:0,1,2,3,4 [2, 3, 1, 0, 3]
+item indeks:semua [2, 3, 1, 0, 3, 1, 1, 1, 6]
+item indeks:1,2,3 [3, 1, 0]
+item indeks:2,3,4 [1, 0, 3]
+item indeks:[1:8] [3, 1, 0, 3, 1, 1, 1]
+data kelas [('agama islam', 2), ('Pengantar Teknologi Informasi', 3), ('Pengantar Pemrograman', 3)]
+data kelas [('agama islam', 2), ('Pengantar Teknologi Informasi', 3), ('Pengantar Pemrograman', 3), ('Bahasa Indonesia', 2)]
+data kelas [('agama islam', 2), ('Pengantar Teknologi Informasi', 3), ('Pengantar Pemrograman', 3), ('Bahasa Indonesia', 2), ('Agama Islam', 3)]
+Nama Mata Kuliah 1: agama islam dengan jumlah sks : 2
+Nama Mata Kuliah 2: Pengantar Teknologi Informasi dengan jumlah sks : 3
+Nama Mata Kuliah 3: Pengantar Pemrograman dengan jumlah sks : 3
+Nama Mata Kuliah 4: Bahasa Indonesia dengan jumlah sks : 2
+Nama Mata Kuliah 5: Agama Islam dengan jumlah sks : 3
+Nama Mahasiswa : muahammad farel shahizidan lefrand
+NIM Mahasiswa : 2 3 1 0 3 1 1 1 6
+Program Pendidikan muahammad farel shahizidan lefrand adalah : Sistem Informasi D S1-Reguler
+Angkatan : 2023 - Ganjil
+Jumlah nilai muahammad farel shahizidan lefrand adalah : 5
+Data terbesar muahammad farel shahizidan lefrand adalah : 100
+Data terkecil muahammad farel shahizidan lefrand adalah : 89
+Rata-rata nilai muahammad farel shahizidan lefrand adalah : 93.8
---
+++ pratikum farel.py
@@ -0,0 +1,28 @@
+Python 3.11.5 (v3.11.5:cce6ba91b3, Aug 24 2023, 10:50:31) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=== RESTART: /Users/muhammadfarelshahizidanlefrand/Downloads/praktikum-d3.py ===
+----------------------------------------
+        Ajeng Niwahintan Pratiwi        
+               231031108                
+
+
+
+                             Praktikum 3
+                      Sistem Informasi D
+           ajenggniwahintanp11@gmail.com
+----------------------------------------
+	Halo, selamat datang perkenalkan nama saya Ajeng Niwahintan Pratiwi dengan NIM 231031108 dari prodi Sistem Informasi D. Ini adalah file Praktikum 3.
+Masukkan Nilai --5++9--=
+=== RESTART: /Users/muhammadfarelshahizidanlefrand/Downloads/praktikum-d3.py ===
+----------------------------------------
+   muhammad farel shahizidan lefrand    
+               231031119                
+
+
+
+                             Praktikum 3
+                      Sistem Informasi D
+                       fawrrel@gmail.com
+----------------------------------------
+	Halo, selamat datang perkenalkan nama saya muhammad farel shahizidan lefrand dengan NIM 231031119 dari prodi Sistem Informasi D. Ini adalah file Praktikum 3.
